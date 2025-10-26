aa287f7fdc6edefa14f3f385dd999976cdaa7d3e
irfanLegion <irfanimran1997@gmail.com>
2025-10-27 01:23:51 +0800

    complete lesson 7 ex 2

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.greenBox}>
+        <Text style={[styles.boxText, styles.title]}>Who We Are</Text>
+      </View>
+      <View style={styles.greenBox}>
+        <Text style={styles.boxText}>Our People</Text>
+      </View>
+      <View style={styles.greenBox}>
+        <Text style={styles.boxText}>Our Campus</Text>
+      </View>
+      <StatusBar barStyle="default" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  greenBox: {
+    width: 100,
+    height: 100,
+    marginTop: 30,
+    backgroundColor: "green",
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  boxText: {
+    textAlign: "center",
+    color: "white",
+  },
+  title: {
+    fontWeight: "bold",
   },
 });
